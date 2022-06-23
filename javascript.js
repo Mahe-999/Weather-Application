5ece29f23c3837c3e43f41d29160bce93bd57a7d
Bandaru Uma Maheswari <79190428+Mahe-999@users.noreply.github.com>
2022-06-23 13:00:21 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -59,17 +59,17 @@ function weatherDetails(info){
         const {temp, feels_like, humidity} = info.main;
 
         if(id == 800){
-            wIcon.src = "Weather Icons/clear.svg";
+            wIcon.src = "clear.svg";
         }else if(id >= 200 && id <= 232){
-            wIcon.src = "Weather Icons/storm.svg";  
+            wIcon.src = "storm.svg";  
         }else if(id >= 600 && id <= 622){
-            wIcon.src = "Weather Icons/snow.svg";
+            wIcon.src = "snow.svg";
         }else if(id >= 701 && id <= 781){
-            wIcon.src = "Weather Icons/haze.svg";
+            wIcon.src = "haze.svg";
         }else if(id >= 801 && id <= 804){
-            wIcon.src = "Weather Icons/cloud.svg";
+            wIcon.src = "cloud.svg";
         }else if((id >= 500 && id <= 531) || (id >= 300 && id <= 321)){
-            wIcon.src = "Weather Icons/rain.svg";
+            wIcon.src = "rain.svg";
         }
         
         weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
